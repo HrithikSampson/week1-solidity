b18b2fc7a72f7600a2ed11658d5aa691b77101de
Hrithik Sampson <hrithikedwardsampson@gmail.com>
2024-04-25 15:29:20 +0530

    Week1 change

--- Helloworld.sol
+++ Helloworld.sol
@@ -1,30 +1,24 @@
-//SPDX-License-Identifier: GPL-3.0
+// SPDX-License-Identifier: GPL-3.0
 pragma solidity >=0.7.0 <0.9.0;
-interface HelloWorldInterface {
-    function helloworld() external view returns (string memory);
-    function setText(string memory newText) external;
-}
-
-contract HelloWorld is HelloWorldInterface {
-
-    uint storedData; // State Variable
-    string private text = "Hello World";
-    constructor(string memory myInitialText) {
-        text = "Something";
-        text = myInitialText;
+contract HelloWorld {
+    string private text;
+    address public owner;
+    constructor() {
+        text = "Hello World";
+        owner = msg.sender;
     }
-
-    function helloworld() public view returns (string memory) {
+    function helloWorld() public view returns (string memory) {
         return text;
     }
-    function setText(string memory newText) public {
+    function setText(string calldata newText) public onlyOwner {
         text = newText;
     }
-    fallback() external {
-        text = "Wrong Call!";
+    function transferOwnership(address newOwner) public onlyOwner {
+        owner = newOwner;
     }
-    receive() external payable {
-        /* receive : fallback only for money receive exists */
-        text = "Thank You!";
+    modifier onlyOwner()
+    {
+        require (msg.sender == owner, "Caller is not the owner");
+        _;
     }
 }
